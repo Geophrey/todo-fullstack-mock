c02acb3efc66b5d663b955b836fb7ca84e241aae
Geophrey <jeffkouevi@gmail.com>
2025-11-14 09:55:04 -0500

    made server connection with frontend

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@
 
 import express from "express"  //imported from "type": "module"
 
+import cors from "cors" //essentially a middleware to allow this server to take get requests i think
+
 const app = express()
 
 const port = 8080 // can put this port in another file and import it
 
+app.use(cors())
+
 app.get(`/`, (req, res) => {
-    res.json(`Hellow World! (from server)`)
+    res.json(`Hello World! (from server)`)
 })
 
 app.listen(port, () => {
