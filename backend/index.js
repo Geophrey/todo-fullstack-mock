921e0da5c49981306e7593961e9fd81c84ebdea3
Geophrey <jeffkouevi@gmail.com>
2025-11-14 09:36:19 -0500

    basic server set-up

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
 //Main server file used for connecting to database etc
 
+import express from "express"  //imported from "type": "module"
+
+const app = express()
+
+const port = 8080 // can put this port in another file and import it
+
+app.get(`/`, (req, res) => {
+    res.json(`Hellow World! (from server)`)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
